3494e873f3d8acce40b0fbd5381539214bff369c
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-04-02 17:46:22 +0100

    fixing bugs and tried adding a home page

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import router from "./routes/index.routes";
 import cors from "cors";
 import connectDB from "./connectDB";
 import dotenv from "dotenv";
+import path from 'path';
 
 dotenv.config();
 
@@ -17,6 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
+
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Define a route to render the home.ejs view
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.use("/api/v1/", router);
 
